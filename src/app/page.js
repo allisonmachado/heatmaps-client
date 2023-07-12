3bf64e197b536c9ead27ba6aa9507cef944ed8cb
Allison Machado <aallison93@gmail.com>
2023-07-12 16:06:43 +0100

    feat: add bootstrap and create calendar component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
+import Calendar from '@/components/calendar'
 import styles from './page.module.css'
 
 export default function Home() {
   
-  return <h1>Hello World</h1>
+  return <Calendar></Calendar>
 }

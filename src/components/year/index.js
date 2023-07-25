5ef1c2834cbb3c6a12998eee84fca74c850e7247
Allison Machado <aallison93@gmail.com>
2023-07-25 17:59:15 +0100

    feat: add calendar correct margin

--- src/components/year/index.js
+++ src/components/year/index.js
@@ -7,7 +7,7 @@ const year = (new Date()).getFullYear();
 export default function Year() {
   return (
     <div className="row">
-      {new simpleRange(12).map(month => <div key={month} className="col-xs-12 col-sm-6 col-lg-4 col-xl-2">
+      {new simpleRange(12).map(month => <div key={month} className="col-xs-12 col-sm-6 col-lg-4 col-xl-2 mb-4">
         <Month year={year} month={month}/>
       </div>)}
     </div>

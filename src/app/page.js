00ae086062799d90ecc56fd64df882e1685aad06
Allison Machado <aallison93@gmail.com>
2023-07-27 17:38:00 +0100

    feat: add button to create a habit

--- src/app/page.js
+++ src/app/page.js
@@ -2,38 +2,48 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Habit</th>
-          <th scope="col">Check</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Study Software Development</td>
-          <td>
-            <Link href="/habits/1">
-              <button type="button" className="btn btn-outline-secondary">
-                View
-              </button>
-            </Link>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Study Software Development</td>
-          <td>
-            <Link href="/habits/2">
-              <button type="button" className="btn btn-outline-secondary">
-                View
-              </button>
-            </Link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <div className="row">
+        <div className="col">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Habit</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Study Software Development</td>
+                <td>
+                  <Link href="/habits/1">
+                    <button type="button" className="btn btn-outline-secondary">
+                      View
+                    </button>
+                  </Link>
+                </td>
+              </tr>
+              <tr>
+                <td>Study Software Development</td>
+                <td>
+                  <Link href="/habits/2">
+                    <button type="button" className="btn btn-outline-secondary">
+                      View
+                    </button>
+                  </Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button type="button" className="btn btn-primary">
+            New Habit
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import "./main.css";
 
 export default function Navbar({ title }) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <span className="navbar-brand">{title}</span>
+        <Link href="/"><span className="navbar-brand">{title}</span></Link>
         <span className="nav-item">Hello Allison</span>
         <span className="nav-item">Sign Out</span>
       </div>

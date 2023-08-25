b3049a50639b3a95edb7a6391bb9f2342a0a25fa
Allison Machado <aallison93@gmail.com>
2023-08-25 07:58:54 +0100

    fix: redirect habit update does not exist

--- src/app/habits/[id]/update/page.js
+++ src/app/habits/[id]/update/page.js
@@ -1,8 +1,13 @@
 import HabitForm from "@/components/habit-form";
 import { findUserHabit } from "@/data/habits";
+import { redirect } from "next/navigation";
 
 export default async function UpdateHabit({ params }) {
   const habit = await findUserHabit(params.id);
+
+  if (!habit) {
+    return redirect("/habits");
+  }
 
   return (
     <>

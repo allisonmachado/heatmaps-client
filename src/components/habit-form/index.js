5d427c3c2ee4316580a093a758716cb2ccd4c655
Allison Machado <aallison93@gmail.com>
2023-09-02 12:01:10 +0100

    refactor: habit form

--- src/components/habit-form/index.js
+++ src/components/habit-form/index.js
@@ -1,10 +1,7 @@
 "use client";
 
-import {
-  COMMUNICATION_ERROR_MESSAGE,
-  DEFAULT_FORM_ERROR_MESSAGE,
-} from "@/utils/constants";
-import { useState, useEffect } from "react";
+import { useAuthForm } from "@/hooks/use-auth-form";
+import { useState } from "react";
 
 export default function HabitForm({ habit }) {
   const isCreateForm = !habit;
@@ -15,80 +12,26 @@ export default function HabitForm({ habit }) {
   );
   const [type, setType] = useState(habit?.type ?? "");
 
-  const [loading, setLoading] = useState(false);
-  const [displayError, setDisplayError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(DEFAULT_FORM_ERROR_MESSAGE);
-
-  useEffect(() => {
-    if (loading) {
-      setDisplayError(false);
-    }
-  }, [loading]);
-
-  const upsertHabit = async ({ title, color, type }) => {
-    setLoading(true);
-    setDisplayError(false);
-
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    const body = JSON.stringify({
-      ...(isCreateForm ? {} : { id: habit.id }),
-      title,
-      color: color.slice(1),
-      ...(isCreateForm ? { type } : {}),
-    });
-
-    const commonRequestOptions = {
-      headers: myHeaders,
-      body,
-      redirect: "follow",
-    };
-
-    const requestOptions = isCreateForm
-      ? { ...commonRequestOptions, method: "POST" }
-      : { ...commonRequestOptions, method: "PUT" };
-
-    const path = isCreateForm ? "/api/habits" : `/api/habits/${habit.id}`;
-
-    try {
-      const response = await fetch(path, requestOptions);
-
-      if (response.redirected) {
-        const redirectUrl = response.url;
-        return (window.location.href = redirectUrl);
-      }
-
-      if (response.status >= 200 && response.status < 300) {
-        return (window.location.href = "/");
-      }
-
-      const result = await response.json();
-
-      setDisplayError(true);
-      setErrorMessage(
-        result?.message?.join("; ") || DEFAULT_FORM_ERROR_MESSAGE
-      );
-      setLoading(false);
-    } catch (error) {
-      setDisplayError(true);
-      setErrorMessage(COMMUNICATION_ERROR_MESSAGE);
-      setLoading(false);
-    }
-  };
+  const { loading, displayError, errorMessage, submitForm } = useAuthForm();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!title || !type || !color) {
-      setDisplayError(true);
-      return;
-    }
+    const requestPath = isCreateForm
+      ? "/api/habits"
+      : `/api/habits/${habit.id}`;
+    const requestMethod = isCreateForm ? "POST" : "PUT";
 
-    upsertHabit({
-      title,
-      color,
-      type,
+    submitForm({
+      requestPath,
+      requestMethod,
+      requestBody: {
+        ...(isCreateForm ? {} : { id: habit.id }),
+        title,
+        color: color.slice(1),
+        ...(isCreateForm ? { type } : {}),
+      },
+      successPath: "/",
     });
   };
 
